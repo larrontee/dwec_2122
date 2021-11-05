245f060d665821993f04a9391819843bd5c4d4d0
larrontee <alum.plarrubiam.2020@iesalixar.org>
2021-11-05 21:25:15 +0100

    ejs

---
+++ U4 - Formularios y eventos/U4T1 - Formulario/Ej1.js
@@ -0,0 +1,11 @@
+// Prepara un formulario diseñado para almacenar los discos con los que
+// trabajamos en el ejercicio del tema anterior (U3T8). Recuerda qué 
+
+// elementos se  almacenaban, y elije el componente de formulario más
+// adecuado:
+// • Nombre del disco.
+// • Grupo de música o cantante.
+// • Año de publicación.
+// • Tipo de música (podrá ser “rock”, “pop”, “punk” o “indie”);
+// • Localización: almacenará un número de estantería.
+// • Prestado: almacenará un valor booleano. Por defecto seráfalse.
